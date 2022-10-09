47e963c7a968e08f894528740af61c437c3e84c0
Josh Levin <morpheus636@morpheus636.com>
2022-10-09 17:34:18 -0400

    fixed indentation

--- src/CoreMgr/cpus.cpp
+++ src/CoreMgr/cpus.cpp
@@ -6,78 +6,79 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <exception>
 
 namespace cpus {
 
 // class NonExistentCoreError
 // Constructor for NonExistentCoreError Exception
 NonExistentCoreError::NonExistentCoreError(int core){
-    std::string message = "NonExistentCoreError: Core" + std::to_string(core) + " does not exist.";
-    // Save the message as char* instead of std::string.
-    this->message = const_cast<char*>(message.c_str()); 
+  std::string message = "NonExistentCoreError: Core" + std::to_string(core) + " does not exist.";
+  // Save the message as char* instead of std::string.
+  this->message = const_cast<char*>(message.c_str()); 
 }
 
 // Get the message from the NonExistentCoreError Exception
 char* NonExistentCoreError::what(){
-    return this->message;
+  return this->message;
 }
 
 
 // class CPU
 // Constructor for CPU class.
 CPU::CPU(std::string model_name, std::list<int> p_cores, std::list<int> e_cores) {
-    this->model_name = model_name;
-    this->short_model = data_utils::StringSplit(this->model_name, " ").back();
-    this->p_cores = p_cores;
-    this->e_cores = e_cores;
+  this->model_name = model_name;
+  this->short_model = data_utils::StringSplit(this->model_name, " ").back();
+  this->p_cores = p_cores;
+  this->e_cores = e_cores;
 }
 
 // Private method of CPU class.
 void CPU::SetCore(int core, bool status) {
-    // Don't take core 0 offline or you'll crash the system.
-    if (core != 0) {
-        // Open the core file.
-        std::string file_path = "/sys/devices/system/cpu/cpu" + std::to_string(core) + "/online";
-        std::ofstream core_file(file_path);
-        if (core_file.is_open()) {
-            // Write the status to the core file.
-            std::string core_file_value;
-            if (status == true) {
-                core_file << "1";
-            }
-            else {
-                core_file << "0";
-            }
-            // Write and close the core file.
-            core_file.flush();
-            core_file.close();
-        }
-        // Throw NonExistentCoreError if the core does not exist.
-        else {
-            throw NonExistentCoreError(core);
-        }
+  // Don't take core 0 offline or you'll crash the system.
+  if (core != 0) {
+    // Open the core file.
+    std::string file_path = "/sys/devices/system/cpu/cpu" + std::to_string(core) + "/online";
+    std::ofstream core_file(file_path);
+    if (core_file.is_open()) {
+      // Write the status to the core file.
+      std::string core_file_value;
+      if (status == true) {
+        core_file << "1";
+      }
+      else {
+        core_file << "0";
+      }
+      // Write and close the core file.
+      core_file.flush();
+      core_file.close();
     }
+    // Throw NonExistentCoreError if the core does not exist.
+    else {
+      throw NonExistentCoreError(core);
+    }
+  }
 }
 
 // Public method of CPU class.
 void CPU::SetPCores(bool status) {
-    std::list<int>::iterator core_iter;
-    for (core_iter = this->p_cores.begin(); core_iter != this->p_cores.end(); core_iter++) {
-        this->SetCore(*core_iter, status);
-    }
+  std::list<int>::iterator core_iter;
+  for (core_iter = this->p_cores.begin(); core_iter != this->p_cores.end(); core_iter++) {
+    this->SetCore(*core_iter, status);
+  }
 }
 
 // Public Function
 CPU DetectCPU() {
-    // Dict of CPU versions.
-    // FIXME - This causes header errors
-    std::map<std::string, CPU> supported_cpus;
-    supported_cpus["12th Gen Intel(R) Core(TM) i7-1260P"] = CPU("12th Gen Intel(R) Core(TM) i7-1260P", {0,1,2,3,4,5,6,7,8}, {9,10,11,12,13,14,15});
+  // Dict of CPU versions.
+  // FIXME - This causes header errors
+  std::map<std::string, CPU> supported_cpus;
+  supported_cpus["12th Gen Intel(R) Core(TM) i7-1260P"] = CPU("12th Gen Intel(R) Core(TM) i7-1260P", {0,1,2,3,4,5,6,7,8}, {9,10,11,12,13,14,15});
 
-    // Load model name value from cpuinfo.
-    std::vector<std::string> model_name_line = data_utils::StringSplit(data_utils::FindLine("/proc/cpuinfo", "model name"), ":");
-	// Return the CPU object from `supported_cpus` based on the model name.
-	CPU processor = supported_cpus[data_utils::StringLTrim(model_name_line.at(1))];
-    return processor;
+  // Load model name value from cpuinfo.
+  std::vector<std::string> model_name_line = data_utils::StringSplit(data_utils::FindLine("/proc/cpuinfo", "model name"), ":");
+  // Return the CPU object from `supported_cpus` based on the model name.
+  CPU processor = supported_cpus[data_utils::StringLTrim(model_name_line.at(1))];
+  return processor;
 }
 } // namespace cpus
--- src/CoreMgr/data_utils.cpp
+++ src/CoreMgr/data_utils.cpp
@@ -9,48 +9,48 @@ const std::string WHITESPACE = " \n\r\t\f\v";
 
 // Public Function
 std::string StringLTrim(const std::string &s) {
-    size_t start = s.find_first_not_of(WHITESPACE);
-    return (start == std::string::npos) ? "" : s.substr(start);
+  size_t start = s.find_first_not_of(WHITESPACE);
+  return (start == std::string::npos) ? "" : s.substr(start);
 }
 
 
 // Public Function
 std::string StringRTrim(const std::string &s) {
-    size_t end = s.find_last_not_of(WHITESPACE);
-    return (end == std::string::npos) ? "" : s.substr(0, end + 1);
+  size_t end = s.find_last_not_of(WHITESPACE);
+  return (end == std::string::npos) ? "" : s.substr(0, end + 1);
 }
 
 
 // Public Function
 std::string StringTrim(const std::string &s) {
-    return StringRTrim(StringLTrim(s));
+  return StringRTrim(StringLTrim(s));
 }
 
 
 // Public function.
 std::string FindLine(std::string file_path, std::string substring) {
-    // Open the file as an input string.
-    std::ifstream file(file_path);
-    // Loop through the file line-by-line and check it for the substring.
-	for (std::string line; std::getline(file, line);) {
-		if (line.find(substring) != std::string::npos) {
-            // Close the file and return the line if the substring is found.
-            file.close();
-            return line;
-        }
-	}
-    // Close the file and return an empty string if the substring isn't found.
-    file.close();
-    std::string line = "";
-    return line;
+  // Open the file as an input string.
+  std::ifstream file(file_path);
+  // Loop through the file line-by-line and check it for the substring.
+  for (std::string line; std::getline(file, line);) {
+    if (line.find(substring) != std::string::npos) {
+      // Close the file and return the line if the substring is found.
+      file.close();
+      return line;
+    }
+  }
+  // Close the file and return an empty string if the substring isn't found.
+  file.close();
+  std::string line = "";
+  return line;
 }
 
 
 // Public function.
 std::vector<std::string> StringSplit(std::string original_string, std::string delimiter) {
-    std::vector<std::string> split_strings;
-    // TODO - Write split_string function
-    return split_strings;
+  std::vector<std::string> split_strings;
+  // TODO - Write split_string function
+  return split_strings;
 }
 
 
